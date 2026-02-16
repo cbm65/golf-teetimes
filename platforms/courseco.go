aad0e6abdd2c3e4e3d764ec73fc1c88a66fe57b8
cmccabe <conor.mccabe@patmos.tech>
2026-02-15 21:59:08 -0700

    add more dallas courses v6

--- platforms/courseco.go
+++ platforms/courseco.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
-	"net/url"
 	"strconv"
 	"strings"
 )
@@ -19,6 +19,8 @@ type CourseCoCourseConfig struct {
 	DisplayName string `json:"displayName"`
 	City        string `json:"city"`
 	State       string `json:"state"`
+	GatewayURL  string `json:"gatewayUrl"`
+	OriginURL   string `json:"originUrl"`
 }
 
 var CourseCoCourses = map[string]CourseCoCourseConfig{}
@@ -37,22 +39,36 @@ type CourseCoTeeTime struct {
 }
 
 func FetchCourseCo(config CourseCoCourseConfig, date string) ([]DisplayTeeTime, error) {
-	var url string = fmt.Sprintf(
-		"https://courseco-gateway.totaleintegrated.net/Booking/Teetimes?IsInitTeeTimeRequest=false&TeeTimeDate=%s&CourseID=%s&StartTime=05:00&EndTime=21:00&NumOfPlayers=-1&Holes=Any&IsNineHole=0&StartPrice=0&EndPrice=&CartIncluded=false&SpecialsOnly=0&IsClosest=0&PlayerIDs=&DateFilterChange=false&DateFilterChangeNoSearch=false&SearchByGroups=true&IsPrepaidOnly=0&QueryStringFilters=null",
-		date, url.QueryEscape(config.CourseID),
+	gatewayBase := config.GatewayURL
+	if gatewayBase == "" {
+		gatewayBase = "https://courseco-gateway.totaleintegrated.net"
+	}
+	origin := config.OriginURL
+	if origin == "" {
+		origin = "https://" + config.Subdomain + ".totaleintegrated.net"
+	}
+
+	// Encode spaces but preserve commas for multi-course IDs
+	escapedCourseID := strings.ReplaceAll(config.CourseID, " ", "%20")
+
+	var apiURL string = fmt.Sprintf(
+		"%s/Booking/Teetimes?IsInitTeeTimeRequest=false&TeeTimeDate=%s&CourseID=%s&StartTime=05:00&EndTime=21:00&NumOfPlayers=-1&Holes=Any&IsNineHole=0&StartPrice=0&EndPrice=&CartIncluded=false&SpecialsOnly=0&IsClosest=0&PlayerIDs=&DateFilterChange=false&DateFilterChangeNoSearch=false&SearchByGroups=true&IsPrepaidOnly=0&QueryStringFilters=null",
+		gatewayBase, date, escapedCourseID,
 	)
+
+	log.Printf("[CourseCo] %s: GET %s", config.Key, apiURL)
 
 	var req *http.Request
 	var err error
-	req, err = http.NewRequest("GET", url, nil)
+	req, err = http.NewRequest("GET", apiURL, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Set("Accept", "application/json, text/plain, */*")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36")
-	req.Header.Set("Origin", "https://"+config.Subdomain+".totaleintegrated.net")
-	req.Header.Set("Referer", "https://"+config.Subdomain+".totaleintegrated.net/")
+	req.Header.Set("Origin", origin)
+	req.Header.Set("Referer", origin+"/")
 
 	var client http.Client
 	var resp *http.Response
@@ -67,6 +83,8 @@ func FetchCourseCo(config CourseCoCourseConfig, date string) ([]DisplayTeeTime, 
 	if err != nil {
 		return nil, err
 	}
+
+	log.Printf("[CourseCo] %s: status %d, body %d bytes", config.Key, resp.StatusCode, len(body))
 
 	var data CourseCoResponse
 	err = json.Unmarshal(body, &data)
@@ -103,6 +121,8 @@ func FetchCourseCo(config CourseCoCourseConfig, date string) ([]DisplayTeeTime, 
 			BookingURL: config.BookingURL,
 		})
 	}
+
+	log.Printf("[CourseCo] %s: returning %d tee times", config.Key, len(results))
 
 	return results, nil
 }
